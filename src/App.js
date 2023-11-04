98e1dd0ed9596e9c5f7438db9b1069c05493fccc
unknown <toliguemi94@gmail.com>
2023-11-04 01:08:50 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
 
 function App() {
+  const [trafficLightState, setLightState] = useState(0) //Default on stop
+  const [purpleHide, setPurpleState] = useState(true)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="mainContainer">
+        <div className="trafficLightHolder"></div>
+        <div className="trafficLightContainer">
+          <div className={`redLightBulb ${trafficLightState === 0 ? "selected" : ""}`} onClick={() => setLightState(0)}></div>
+          <div className={`yellowLightBulb ${trafficLightState === 1 ? "selected" : ""}`} onClick={() => setLightState(1)}></div>
+          <div className={`greenLightBulb ${trafficLightState === 2 ? "selected" : ""}`} onClick={() => setLightState(2)}></div>
+          <div className={`purpleLightBulb ${trafficLightState === 3 ? "selected" : ""} ${purpleHide === true ? "hideLight" : ""}`} onClick={() => setLightState(3)}></div>
+        </div>
+        <button className="nextLight" onClick={() => setLightState((trafficLightState + 1) % (purpleHide === true ? 3 : 4))}>Next Light</button>
+        <button className="addLightButton" onClick={() => setPurpleState(false)}>Add an extra light</button>
+      </div>
+    </div >
+
+  )
 }
 
 export default App;
